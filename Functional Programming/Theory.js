d04f602eb1f3f9d769f7b6199e2ec95b0496dd4a
Bryan Ferreira <bryandfh@gmail.com>
2019-11-20 15:09:46 +0100

    Functional programming added

---
+++ Functional Programming/Theory.js
@@ -0,0 +1,16 @@
+/* 
+Functional programming is an approach to software development based around the evaluation of functions. 
+Functions in programming map input to output to produce a result. You can combine basic functions in many ways to build more and more complex programs.
+The functional programming approach breaks a program into small, testable parts.
+
+Functional programming follows a few core principles:
+1) Functions are independent from the state of the program or global variables. They only depend on the arguments passed into them to make a calculation
+2) Functions try to limit any changes to the state of the program and avoid changes to the global objects holding data
+3)Functions have minimal side effects in the program
+
+Functional programming is about:
+1) Isolated functions: There is no dependence on the state of the program, which includes global variables that are subject to change
+2) Pure functions: The same input always gives the same output
+3) Functions with limited side effects: Any changes, or mutations, to the state of the program outside the function are carefully controlled
+
+*/
