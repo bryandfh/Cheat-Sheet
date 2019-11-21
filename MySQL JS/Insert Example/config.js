b23a08efc7287782b4ccbbca0b98cb5b58932639
Bryan Ferreira <bryandfh@gmail.com>
2019-11-21 15:27:28 +0100

    SQL Insert Example

---
+++ MySQL JS/Insert Example/config.js
@@ -0,0 +1,8 @@
+let config = {
+    host    : 'localhost',
+    user    : 'bryan',
+    password: 'test123',
+    database: 'german'
+};
+
+module.exports = config;
---
+++ MySQL JS/Insert Example/insert.js
@@ -0,0 +1,39 @@
+let mysql = require('mysql');
+let config = require('./config.js');
+let connection = mysql.createConnection(config);
+
+let fs = require('fs');
+
+function parse(row){
+    row = row.split(' – ')
+    row = row[1];
+    row = row.split('~ ')
+
+    let plural = row[1].split('\r')[0];
+    let singular = row[0].split(' ')[0] + " " + row[0].split(' ')[1];
+    let artikel = row[0].split(' ')[0];
+    return [artikel, singular, plural];    
+}
+
+function getData(fileLocation){
+    let array = fs.readFileSync(fileLocation).toString().split("\n");
+    let dict = [];
+    for ( i in array) {
+        dict.push(parse(array[i]));
+    }
+    return dict; //[[artikel, singular, plural], [artikel, singular, plural] , [artikel, singular, plural], ...]
+}
+
+//The data need to be in this format [[[artikel, singular, plural], [artikel, singular, plural] , [artikel, singular, plural], ...]]
+//Explanation: https://github.com/mysqljs/mysql#escaping-query-values
+//Bref explanation: Nested arrays are turned into grouped lists (for bulk inserts), e.g. [['a', 'b'], ['c', 'd']] turns into ('a', 'b'), ('c', 'd')
+
+connection.connect(function(error) {
+    if (error) throw error;
+    let sql = "INSERT INTO artikel (Artikel, Singular, Plural) VALUES ?";
+    let values = getData('./artikel.txt');
+    connection.query(sql, [values], function(error) {
+        if (error) throw error;
+        connection.end();
+    });
+});
