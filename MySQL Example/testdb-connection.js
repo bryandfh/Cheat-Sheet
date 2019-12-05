b9dd803e7cff64f2d88a2ccfd042644544099d70
Bryan Ferreira <bryandfh@gmail.com>
2019-12-05 15:02:05 +0100

    Basic js for testing connection

---
+++ MySQL Example/testdb-connection.js
@@ -0,0 +1,13 @@
+let mysql = require('mysql');
+let config = require('./config.js');
+
+let connection = mysql.createConnection(config);
+connection.connect(function(error) {
+    if (error) throw error;
+    let sql = "SHOW DATABASES";
+    connection.query(sql, function(error, result) {
+        if (error) throw error;
+        console.log(result);
+        connection.end();
+    });
+});
